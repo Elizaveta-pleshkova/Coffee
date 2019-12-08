9380cdb0e3e7d9ee6e43d377c98e3b3d150bf244
Elizaveta-pl <lisavet02@gmail.com>
2019-12-08 16:26:52 +0400

    ready

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
-from main.ui import Ui_mainWindow
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
 import sqlite3
 import sys
 
 
-def fill():
-    ui.tableWidget.clear()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.pushButton.clicked.connect(self.fill)
 
-    labels = ['ID', 'Название сорта', 'Степень обжарки', 'Молотый/в зернах',
-              'Описание вкуса',  'Цена',  'Объем упаковки']
+    def fill(self):
+        self.tableWidget.clear()
 
-    ui.tableWidget.setColumnCount(len(labels))
-    ui.tableWidget.setHorizontalHeaderLabels(labels)
-    with sqlite3.connect("Coffee.db") as connect:
-        for ID, Name, stepen, Tip, Opisanie, Cena, Obem in connect.execute(
-                """SELECT * FROM prise where id > 0"""):
-            row = self.tableWidget.rowCount()
-            ui.tableWidget.setRowCount(row + 1)
-            ui.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(str(ID)))
-            ui.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(Name))
-            ui.tableWidget.setItem(row, 2, QtWidgets.QTableWidgetItem(stepen))
-            ui.tableWidget.setItem(row, 3, QtWidgets.QTableWidgetItem(Tip))
-            ui.tableWidget.setItem(row, 4, QtWidgets.QTableWidgetItem(Opisanie))
-            ui.tableWidget.setItem(row, 5, QtWidgets.QTableWidgetItem(str(Cena)))
-            ui.tableWidget.setItem(row, 6, QtWidgets.QTableWidgetItem(str(Obem)))
+        labels = ['ID', 'Название сорта', 'Степень обжарки', 'Молотый/в зернах',
+                  'Описание вкуса', 'Цена', 'Объем упаковки']
+
+        self.tableWidget.setColumnCount(len(labels))
+        self.tableWidget.setHorizontalHeaderLabels(labels)
+        with sqlite3.connect("coffee.db") as connect:
+            for ID, Name, Stepen, Tip, Opisanie, Cena, Obem in connect.execute(
+                    """SELECT * FROM prise where id > 0"""):
+                row = self.tableWidget.rowCount()
+                self.tableWidget.setRowCount(row + 1)
+                self.tableWidget.setItem(row, 0, QTableWidgetItem(str(ID)))
+                self.tableWidget.setItem(row, 1, QTableWidgetItem(Name))
+                self.tableWidget.setItem(row, 2, QTableWidgetItem(Stepen))
+                self.tableWidget.setItem(row, 3, QTableWidgetItem(Tip))
+                self.tableWidget.setItem(row, 4, QTableWidgetItem(Opisanie))
+                self.tableWidget.setItem(row, 5, QTableWidgetItem(str(Cena)))
+                self.tableWidget.setItem(row, 6, QTableWidgetItem(str(Obem)))
 
 
-
-app = QtWidgets.QApplication(sys.argv)
-mainWindow = QtWidgets.QMainWindow()
-ui = Ui_mainWindow()
-ui.setupUi(mainWindow)
-ui.pushButton.clicked.connect(fill)
-mainWindow.show()
+app = QApplication(sys.argv)
+ui = MainWindow()
+ui.show()
 sys.exit(app.exec_())
